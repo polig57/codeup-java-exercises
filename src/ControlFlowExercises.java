0af52fa995a18c964eb32d7b1d390ba257ebffdb
Poli Gonzalez <gonzalezpoli10@gmail.com>
2023-12-22 20:14:24 -0600

    While and Do While Loops Exercise Complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,57 @@
 public class ControlFlowExercises {
    public static void main(String[] args) {
+//---------------------------While Loop------------------------------
+//     {
+        // Create an integer variable i with a value of 5.
+//           int i = 5;
+        // Create a while loop that runs so long as i is less than or equal to 15
+//           while (i <= 15) {
+                // Each loop iteration, output the current value of i, then increment i by one.
+//               System.out.println(i);
+//               i++;
+//      }
+       {
+//---------------------------Do While Loop------------------------------
+//Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//               int i = 0;
+//           do {
+//               System.out.println(i);
+//               i += 2;
+//           } while (i <= 100);
 
+//Alter your loop to count backwards by 5's from 100 to -10.
+
+//Create a do-while loop that starts at 2, and displays the number squared on each line while the number is
+// less than 1,000,000. Output should equal:
+
+
+//---------------------------For Loop------------------------------
+//Refactor the previous two exercises to use a for loop instead
+
+
+
+//---------------------------Fizz Buzz------------------------------
+// One of the most common interview questions for entry-level programmers is the FizzBuzz test.
+// Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+
+    //Write a program that prints the numbers from 1 to 100.
+    //For multiples of three: print “Fizz” instead of the number.
+    //For the multiples of five: print “Buzz”.
+    //For numbers which are multiples of both three and five: print “FizzBuzz”.
+
+//---------------------------Table of Powers------------------------------
+    //Prompt the user to enter an integer.
+    //Display a table of squares and cubes from 1 to the value entered.
+    //Ask if the user wants to continue.
+    //Assume that the user will enter valid data.
+    //Only continue if the user agrees to.
+
+//---------------------------Convert given number grades into letter grades------------------------------
+    //Prompt the user for a numerical grade from 0 to 100.
+    //Display the corresponding letter grade.
+    //Prompt the user to continue.
+    //Assume that the user will enter valid integers for the grades.
+    //The application should only continue if the user agrees to.
+           }
     }
 }
