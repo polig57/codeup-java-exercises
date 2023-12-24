46150df8e96aef0574690ae11a48201ee8000f0c
Poli Gonzalez <gonzalezpoli10@gmail.com>
2023-12-24 16:36:15 -0600

    Up to For Loops Exercise Complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -13,20 +13,55 @@ public class ControlFlowExercises {
        {
 //---------------------------Do While Loop------------------------------
 //Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//           {
 //               int i = 0;
-//           do {
-//               System.out.println(i);
-//               i += 2;
-//           } while (i <= 100);
+//               do {
+//                   System.out.println(i);
+//                   i += 2;
+//               } while (i <= 100);
+//           }
 
 //Alter your loop to count backwards by 5's from 100 to -10.
+//           {
+//               int i = 100;
+//               do {
+//                   System.out.println(i);
+//                   i -= 5;
+//               } while (i >= -10);
+//           }
 
-//Create a do-while loop that starts at 2, and displays the number squared on each line while the number is
-// less than 1,000,000. Output should equal:
-
+//Create a do-while loop that starts at 2, and displays the
+// number squared on each line while the number is less than 1,000,000.
+        //Needed to make int variable into a long variable to hold more numerical value
+//           {
+//               long i = 2;
+//               do {
+//                   System.out.println(i);
+//                   i *= i;
+//               } while (i < 1000000);
+//           }
 
 //---------------------------For Loop------------------------------
 //Refactor the previous two exercises to use a for loop instead
+//         #1
+//           {
+//               for (int i = 5; i <= 15; i ++) {
+//                   System.out.println(i);
+//               }
+//           }
+//         #2
+//           {
+//              { for (int i = 0; i <= 100; i += 2) {
+//                   System.out.println(i);
+//                } 
+//               }
+//                {
+//                    for (int i = 100; i >= -10; i-= 5) {
+//                        System.out.println(i);
+//                    }
+//                }
+                
+//           }
 
 
 
