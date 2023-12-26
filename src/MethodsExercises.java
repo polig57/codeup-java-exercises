16e9a647a596d7e76db47914d2bc1ebc52e8b83e
Poli Gonzalez <gonzalezpoli10@gmail.com>
2023-12-26 15:00:52 -0600

    Initialized Methods Exercise; Completed Addition Method

---
+++ src/MethodsExercises.java
@@ -0,0 +1,9 @@
+public class MethodsExercises {
+    public static long getAddition (int num1, int num2) {
+        return num1 + num2;
+    }
+    public static void main(String[] args) {
+        System.out.println(getAddition(1, 1));
+    }
+
+}
