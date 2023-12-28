9194b458fbea8d4404a10c3928c836f8c9d17a09
Poli Gonzalez <gonzalezpoli10@gmail.com>
2023-12-27 23:58:40 -0600

    Completed Addition-Division Methods

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,8 +2,20 @@ public class MethodsExercises {
     public static long getAddition (int num1, int num2) {
         return num1 + num2;
     }
+    public static long getSubtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+    public static long getMultiplication(long num1, long num2) {
+        return num1 * num2;
+    }
+    public static long getDivision(long num1, long num2) {
+        return num1 / num2;
+    }
     public static void main(String[] args) {
-        System.out.println(getAddition(1, 1));
+        System.out.println(getAddition(1, 5));
+        System.out.println(getSubtraction(1, 5));
+        System.out.println(getMultiplication(1, 5));
+        System.out.println(getDivision(1, 5));
     }
 
 }
